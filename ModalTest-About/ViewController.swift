cfaab39808cae8d4cb601c66ed7739cf19992327
Jason Zhang <jason.zhang@ablevets.com>
2016-05-13 15:35:52 -0400

    Initial Commit

---
+++ ModalTest-About/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalTest-About
+//
+//  Created by AV-ZhangJ on 5/13/16.
+//  Copyright © 2016 AV-ZhangJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ModalTest-AboutTests/ModalTest_AboutTests.swift
@@ -0,0 +1,36 @@
+//
+//  ModalTest_AboutTests.swift
+//  ModalTest-AboutTests
+//
+//  Created by AV-ZhangJ on 5/13/16.
+//  Copyright © 2016 AV-ZhangJ. All rights reserved.
+//
+
+import XCTest
+@testable import ModalTest_About
+
+class ModalTest_AboutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
